55bdd76b0007b0639dd915f2fe5c1bf219b923d7
jamesxu0 <jest034@gmail.com>
2018-09-13 01:15:53 -0400

    Added typed.js

--- js/scripts.js
+++ js/scripts.js
@@ -14,3 +14,13 @@ jQuery(function ($) {
 }); // JQuery end
 
 new WOW().init();
+
+var options = {
+  strings: ["", "I am a coder", "I am a web developer", "I am an app developer", "I am hard-working", "I am a team player", "I love technology", "I am a wolverine"],
+  typeSpeed: 80,
+  loop: true,
+  showCursor: true,
+  backSpeed: 100,
+}
+
+var typed = new Typed("#typed", options);
